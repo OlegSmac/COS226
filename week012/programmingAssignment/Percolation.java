53b2115e27a3ef352795507487caf05788fe49e1
OlegSmac <oleansmacinih@gmail.com>
2021-06-16 08:15:14 +0200

    week1

--- week012/programmingAssignment/Percolation.java
+++ week012/programmingAssignment/Percolation.java
@@ -24,6 +24,9 @@ public class Percolation {
         for (int i = 0; i < op.length; i++) {
             op[i] = false;
         }
+
+        op[op.length - 2] = true;
+        op[op.length - 1] = true;
     }
 
     private int place(int row, int col) {
@@ -32,8 +35,8 @@ public class Percolation {
 
     public void open(int row, int col) {
         int pl = place(row - 1, col - 1);
-        if (pl < 0 || pl >= n * n) {
-            //throw new IllegalArgumentException();
+        if (row <= 0 || col <= 0 || row > n || col > n) {
+            throw new java.lang.IllegalArgumentException();
         }
         if (isOpen(row, col)) {
             return;
@@ -42,37 +45,47 @@ public class Percolation {
             op[pl] = true;
         }
 
-        if (row - 2 >= 0 && col - 1 >= 0 && op[place(row - 2, col - 1)]) { // top
+        if (row - 2 >= 0 && op[place(row - 2, col - 1)]) { // top
             w.union(place(row - 2, col - 1), pl);
         }
 
-        if (row < n && col - 1 < n && op[place(row, col - 1)]) { // low
+        if (row < n && op[place(row, col - 1)]) { // low
             w.union(place(row,col - 1), pl);
         }
 
-        if (row - 1 >= 0 && col - 2 >= 0 && op[place(row - 1, col - 2)]) { // left
+        if (col - 2 >= 0 && op[place(row - 1, col - 2)]) { // left
             w.union(place(row - 1, col - 2), pl);
         }
 
-        if (row - 1 < n && col < n && op[place(row - 1, col)]) { // right
+        if (col < n && op[place(row - 1, col)]) { // right
             w.union(place(row - 1, col), pl);
+        }
+
+        if (row == 1) {
+            w.union(id[id.length - 2], place(0,col - 1)); // high row
         }
     }
 
     public boolean isOpen(int row, int col) {
         int pl = place(row - 1, col - 1);
-        if (pl < 0 || pl >= n * n) {
-            //throw new IllegalArgumentException();
+        if (row <= 0 || col <= 0 || row > n || col > n) {
+            throw new java.lang.IllegalArgumentException();
         }
         return op[pl];
     }
 
     public boolean isFull(int row, int col) {
         int pl = place(row - 1, col - 1);
-        if (pl < 0 || pl >= n * n) {
-            //throw new IllegalArgumentException();
+        if (row <= 0 || col <= 0 || row > n || col > n) {
+            throw new java.lang.IllegalArgumentException();
         }
-        if (op[pl] && w.find(pl) == w.find(id.length - 2)) {
+
+        if (isOpen(row, col) && row == n) {
+            w.union(pl, id[id.length - 1]);
+        }
+
+        if (isOpen(row, col) && w.find(pl) == id.length - 2) {
+            StdOut.println("find(" + row + ", " + col + ") = " + w.find(pl));
             return true;
         }
         return false;
@@ -80,7 +93,7 @@ public class Percolation {
 
     public int numberOfOpenSites() {
         int count = 0;
-        for (int i = 0; i < op.length; i++) {
+        for (int i = 0; i < op.length - 2; i++) {
             if (op[i]) {
                 count++;
             }
@@ -89,7 +102,12 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return w.find(id[id.length - 2]) == w.find(id[id.length - 1]);
+        for (int i = 0; i < n; i++) {
+            if (isOpen(n, i + 1) && isFull(n, i + 1)) {
+                //return true;
+            }
+        }
+        return w.connected(id[id.length - 2], id[id.length - 1]);
     }
 
     public static void main(String[] main) {
@@ -98,15 +116,6 @@ public class Percolation {
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
-            per.open(6, 7);
-            //per.open(6, 8);
-            if (p == 1) {
-                per.w.union(per.id[per.id.length - 2], per.place(0, q - 1)); // high row
-            }
-            if (p == n) {
-                per.w.union(per.id[per.id.length - 1], per.place(p - 1, q - 1)); // low row
-            }
-            //StdOut.println("isFull(p, q) = " + per.isFull(p, q));
             per.open(p, q);
 
             StdOut.println("p = " + p + " q = " + q);
--- week012/programmingAssignment/PercolationStats.java
+++ week012/programmingAssignment/PercolationStats.java
@@ -1,42 +1,43 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
     private int n, T;
-    private double[] x = new double[T]; // fraction of open sites
+    private double[] x; // fractions of open sites
 
     public PercolationStats(int n, int trials) {
         this.n = n;
         this.T = trials;
         if (n <= 0 || T <= 0) {
-            throw new IllegalArgumentException();
+            throw new java.lang.IllegalArgumentException();
         }
+        x = new double[T];
 
         for (int i = 0; i < T; i++) {
+            //StdOut.println("i = " + i);
             Percolation grid = new Percolation(n);
-            int row = StdRandom.uniform(n);
-            int col = StdRandom.uniform(n);
-            //StdOut.println("row = " + row);
-            //StdOut.println("col = " + col);
             while (!grid.percolates()) {
-                while(grid.isOpen(row, col)) {
-                    row = StdRandom.uniform(n);
-                    col = StdRandom.uniform(n);
+                int place = StdRandom.uniform(n * n);
+                int row = place / n;
+                int col = place % n;
+                //StdOut.println("place = " + place + " row = " + row + " col = " + col);
+                if (!grid.isOpen(row + 1, col + 1)) {
+                    //StdOut.println("open place(" + row + ", " + col + ")");
+                    grid.open(row + 1, col + 1);
                 }
-                grid.open(col, row);
             }
 
-            int openSites = grid.numberOfOpenSites();
-            x[i] = openSites * 1.0 / (n * n);
+            x[i] = 1.0 * grid.numberOfOpenSites() / (n * n);
         }
     }
 
     public double mean() {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < x.length; i++) {
             sum += x[i];
         }
-        return sum * 1.0 / T;
+        return (sum / T);
     }
 
     private double sqr(double n) {
@@ -44,15 +45,15 @@ public class PercolationStats {
     }
 
     public double stddev() {
-        double qsum = 0;
+        double s = 0;
         for (int i = 0; i < x.length; i++) {
-            qsum += sqr(x[i] - mean());
+            s += sqr(x[i] - mean());
         }
-        return Math.sqrt(qsum / (T - 1));
+        return Math.sqrt(s / (T - 1));
     }
 
     public double confidenceLo() {
-        return mean() - (1.96 * stddev() / Math.sqrt(T));
+        return StdStats.mean(x) - (1.96 * StdStats.stddev(x) / Math.sqrt(T));
     }
 
     public double confidenceHi() {
@@ -63,8 +64,9 @@ public class PercolationStats {
         int n = Integer.parseInt(args[0]);
         int T = Integer.parseInt(args[1]);
         PercolationStats a = new PercolationStats(n, T);
-        StdOut.println("mean                    = " + a.mean());
-        StdOut.println("stddev                  = " + a.stddev());
+
+        StdOut.println("mean                    = " + StdStats.mean(a.x));
+        StdOut.println("stddev                  = " + StdStats.stddev(a.x));
         StdOut.println("95% confidence interval = [" + a.confidenceLo() + ", " + a.confidenceHi() + "]");
     }
 }
