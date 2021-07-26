fc1ed895876f67a7072c5f4152a73d4017d6cdcf
OlegSmac <oleansmacinih@gmail.com>
2021-07-26 19:22:17 +0200

    week3

--- week03/programmingAssignment/FastCollinearPoints.java
+++ week03/programmingAssignment/FastCollinearPoints.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.LinearProbingHashST;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -7,58 +8,53 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class FastCollinearPoints {
-    private int numOfSegments;
-    private LineSegment[] seg;
-    private double[] slopes;
+    private ArrayList<LineSegment> seg = new ArrayList<>();
 
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
         }
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null) { throw new IllegalArgumentException(); }
-            for (int j = 0; j < i; j++) {
-                if (points[i].compareTo(points[j]) == 0) {
-                    throw new IllegalArgumentException();
-                }
-            }
-        }
 
         ComparePoints comparePoints = new ComparePoints();
-        ArrayList<Point> colPoints = new ArrayList<>();
-        ArrayList<ArrayList<Point>> allColPoints = new ArrayList<ArrayList<Point>>();
-        seg = new LineSegment[points.length];
+        ArrayList<Point> allColPoints = new ArrayList<Point>();
         for (int p = 0; p < points.length; p++) {
-            slopes = new double[points.length];
-            Point[] copyPoints = new Point[points.length];
+            if (points[p] == null) {
+                throw new IllegalArgumentException();
+            }
+            double[] slopes = new double[points.length];
+            Point[] copyPoints = points.clone();
             for (int q = 0; q < points.length; q++) {
-                slopes[q] = points[p].slopeTo(points[q]);
-                copyPoints[q] = points[q];
+                slopes[q] = points[q].slopeTo(points[p]);
             }
             sorting(slopes, copyPoints);
             int n = 0;
+            ArrayList<Point> colPoints = new ArrayList<>();
             for (int i = 1; i < slopes.length; i++) {
+                if (slopes[i] == Double.NEGATIVE_INFINITY) {
+                    throw new IllegalArgumentException();
+                }
                 if (slopes[i] == slopes[i - 1]) {
-                    if (n == 0) {
+                    if (colPoints.isEmpty()) {
                         colPoints.add(points[p]);
                         colPoints.add(copyPoints[i - 1]);
                     }
                     colPoints.add(copyPoints[i]);
                     n++;
-                    if (n >= 2) {
-                        colPoints.sort(comparePoints);
-                        if (allColPoints.contains(colPoints)) {
-                            break;
-                        }
-                        else {
-                            allColPoints.add(colPoints);
-                            seg[numOfSegments++] = new LineSegment(colPoints.get(0), colPoints.get(n + 1));
-                        }
-                    }
                 }
-                else {
+                else if (slopes[i] != slopes[i - 1] && n < 2) {
+                    colPoints.clear();
                     n = 0;
-                    colPoints = new ArrayList<>();
+                }
+                if ((slopes[i] != slopes[i - 1] && n >= 2) ||
+                        (i == slopes.length - 1 && n >= 2)) {
+                    colPoints.sort(comparePoints);
+                    if (!contain(allColPoints, colPoints)) {
+                        seg.add(new LineSegment(colPoints.get(0), colPoints.get(n + 1)));
+                        allColPoints.add(colPoints.get(0));
+                        allColPoints.add(colPoints.get(n + 1));
+                    }
+                    colPoints.clear();
+                    n = 0;
                 }
             }
         }
@@ -83,14 +79,23 @@ public class FastCollinearPoints {
         }
     }
 
+    private boolean contain(ArrayList<Point> all, ArrayList<Point> colPoints) {
+        for (int i = 0; i < all.size(); i += 2) {
+            if (all.get(i) == colPoints.get(0) && all.get(i + 1) == colPoints.get(colPoints.size() - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int numberOfSegments() {
-        return numOfSegments;
+        return seg.size();
     }
 
     public LineSegment[] segments() {
-        LineSegment[] res = new LineSegment[numOfSegments];
-        for (int i = 0; i < numOfSegments; i++) {
-            res[i] = seg[i];
+        LineSegment[] res = new LineSegment[seg.size()];
+        for (int i = 0; i < seg.size(); i++) {
+            res[i] = seg.get(i);
         }
         return res;
     }
