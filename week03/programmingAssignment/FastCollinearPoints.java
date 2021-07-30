4d4add9afced7337484bc6b9c30b15e22b32f4a8
OlegSmac <oleansmacinih@gmail.com>
2021-07-30 18:01:09 +0200

    week3

--- week03/programmingAssignment/FastCollinearPoints.java
+++ week03/programmingAssignment/FastCollinearPoints.java
@@ -1,15 +1,12 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.LinearProbingHashST;
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdOut;
-
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class FastCollinearPoints {
-    private ArrayList<LineSegment> seg = new ArrayList<>();
+    private static final double EPS = 1E-12;
+    private LineSegment[] segmentsArray;
 
+    // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
@@ -19,116 +16,72 @@ public class FastCollinearPoints {
                 throw new IllegalArgumentException();
             }
         }
-
-        ComparePoints comparePoints = new ComparePoints();
-        ArrayList<Point> allColPoints = new ArrayList<Point>();
-        for (int p = 0; p < points.length; p++) {
-            double[] slopes = new double[points.length];
-            Point[] copyPoints = points.clone();
-            for (int q = 0; q < points.length; q++) {
-                slopes[q] = points[q].slopeTo(points[p]);
-            }
-            sorting(slopes, copyPoints);
-            int n = 0;
-            ArrayList<Point> colPoints = new ArrayList<>();
-            for (int i = 1; i < slopes.length; i++) {
-                if (slopes[i] == Double.NEGATIVE_INFINITY) {
-                    throw new IllegalArgumentException();
+        if (containsDuplicate(points)) {
+            throw new IllegalArgumentException();
+        }
+        List<LineSegment> segmentList = new ArrayList<LineSegment>();
+        for (int j = 0; j < points.length; j++) {
+            Point[] cands = new Point[points.length - 1];
+            for (int k = 0; k < points.length; k++) {
+                if (k != j) {
+                    if (k < j)
+                        cands[k] = points[k];
+                    else
+                        cands[k - 1] = points[k];
                 }
-                if (slopes[i] == slopes[i - 1]) {
-                    if (colPoints.isEmpty()) {
-                        colPoints.add(points[p]);
-                        colPoints.add(copyPoints[i - 1]);
+            }
+
+            Arrays.sort(cands, points[j].slopeOrder());
+            for (int i = 0; i < cands.length;) {
+                int start = i;
+                Point minPoint = points[j];
+                Point maxPoint = points[j];
+                double slope = cands[start].slopeTo(points[j]);
+                for (; i < cands.length; i++) {
+                    if (!doubleEquals(slope, cands[i].slopeTo(points[j]), EPS))
+                        break;
+
+                    if (cands[i].compareTo(minPoint) < 0) {
+                        minPoint = cands[i];
                     }
-                    colPoints.add(copyPoints[i]);
-                    n++;
-                }
-                else if (slopes[i] != slopes[i - 1] && n < 2) {
-                    colPoints.clear();
-                    n = 0;
-                }
-                if ((slopes[i] != slopes[i - 1] && n >= 2) ||
-                        (i == slopes.length - 1 && n >= 2)) {
-                    colPoints.sort(comparePoints);
-                    if (!contain(allColPoints, colPoints)) {
-                        seg.add(new LineSegment(colPoints.get(0), colPoints.get(n + 1)));
-                        allColPoints.add(colPoints.get(0));
-                        allColPoints.add(colPoints.get(n + 1));
+
+                    if (cands[i].compareTo(maxPoint) > 0) {
+                        maxPoint = cands[i];
                     }
-                    colPoints.clear();
-                    n = 0;
+                }
+
+                if (minPoint == points[j] && i - start > 2) {
+                    segmentList.add(new LineSegment(minPoint, maxPoint));
                 }
             }
         }
+
+        this.segmentsArray = new LineSegment[segmentList.size()];
+        segmentList.toArray(this.segmentsArray);
     }
 
-    private void sorting(double[] sl, Point[] points) {
-        for (int i = 1; i < sl.length; i++) {
-            for (int j = i; j > 0 && sl[j] < sl[j - 1]; j--) {
-                double swap = sl[j];
-                sl[j] = sl[j - 1];
-                sl[j - 1] = swap;
-                Point s = points[j];
-                points[j] = points[j - 1];
-                points[j - 1] = s;
-            }
-        }
+    private boolean doubleEquals(double d1, double d2, double eps) {
+        return d1 == d2 || Math.abs(d1 - d2) < eps;
     }
 
-    private class ComparePoints implements Comparator<Point> {
-        public int compare(Point i, Point j) {
-            return i.compareTo(j);
-        }
-    }
-
-    private boolean contain(ArrayList<Point> all, ArrayList<Point> colPoints) {
-        for (int i = 0; i < all.size(); i += 2) {
-            if (all.get(i) == colPoints.get(0) && all.get(i + 1) == colPoints.get(colPoints.size() - 1)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    // the number of line segments
     public int numberOfSegments() {
-        return seg.size();
+        return this.segmentsArray.length;
     }
 
+    // the line segments
     public LineSegment[] segments() {
-        LineSegment[] res = new LineSegment[seg.size()];
-        for (int i = 0; i < seg.size(); i++) {
-            res[i] = seg.get(i);
-        }
-        return res;
+        return this.segmentsArray.clone();
     }
 
-    public static void main(String[] args) {
-
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
+    private static boolean containsDuplicate(Point[] points) {
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0)
+                    return true;
+            }
         }
 
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
+        return false;
     }
 }
